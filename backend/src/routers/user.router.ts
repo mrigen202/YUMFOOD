b66841305a838613adf9c3ef26b0aa670b0a509b
Mrigen Boro <mrigend14@gmail.com>
2025-01-02 10:10:07 -0800

    FOODPROJECT

---
+++ backend/src/routers/user.router.ts
@@ -0,0 +1,76 @@
+import {Router} from 'express';
+import { sample_users } from '../data';
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import { User, UserModel } from '../models/user.model';
+import { HTTP_BAD_REQUEST } from '../constants/http_status';
+import bcrypt from 'bcryptjs';
+const router = Router();
+
+router.get("/seed", asyncHandler(
+  async (req, res) => {
+     const usersCount = await UserModel.countDocuments();
+     if(usersCount> 0){
+       res.send("Seed is already done!");
+       return;
+     }
+ 
+     await UserModel.create(sample_users);
+     res.send("Seed Is Done!");
+ }
+ ))
+
+router.post("/login",(req, res) => {
+    const {email, password} = req.body;
+    const user = sample_users.find(user => user.email === email
+      && user.password === password);
+  
+     if(user){
+      res.send(generateTokenReponse(user));
+     }
+     else{
+       res.status(HTTP_BAD_REQUEST).send("Username or password is invalid!");
+     }
+  
+  }
+)
+  
+router.post('/register', asyncHandler(
+  async (req, res) => {
+    const {name, email, password, address} = req.body;
+    const user = await UserModel.findOne({email});
+    if(user){
+      res.status(HTTP_BAD_REQUEST)
+      .send('User is already exist, please login!');
+      return;
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser:User = {
+      id: '',
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+      token: ''
+    }
+
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenReponse(dbUser));
+  }
+))
+
+  const generateTokenReponse = (user : any) => {
+    const token = jwt.sign({
+     id: user.id, email:user.email, isAdmin: user.isAdmin
+    },"some random text",{
+      expiresIn:"30d"
+    });
+  user.token = token;
+    return user;
+  }
+  
+
+  export default router;
